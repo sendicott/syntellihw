6b0a2d2df4d338f9f2e3e5a6d43996c702c51c8a
Scott Endicott <sendicott1391@gmail.com>
2016-10-31 14:09:52 -0400

    Created bar charts with method chains and linear scales

--- app.js
+++ app.js
@@ -1,46 +1,98 @@
 let data = [4, 8, 15, 16, 23, 42];
 
+//---------------Appending text to DOM within divs---------------
 // window.addEventListener("load", function() {
-//     let section = d3.selectAll("section");
+//     let div = d3.select("div");
 
-//     section.append("div")
+//     div.append("section")
 //         .html("Hello, world")
 //         .style("color", "orange")
 //         .style("font-size", "44px");
 
-//     section.append("div")
-//         .html("Goodbye, world");
+//     div.append("section")
+//         .html("Happy Halloween");
 
-//     section.append("div")
+//     div.append("section")
 //         .html("Hello again!");
 
-//     section.append("div")
-//         .html("Okay goodbye for real this time");
+//     div.append("section")
+//         .html("Happy November");
 // });
+//---------------Appending text to DOM within divs---------------
 
+//---------------Basic Bar Chart---------------
 // window.addEventListener("load", function() {
 //     d3.select(".chart")
 //         .selectAll("div")
 //             .data(data)
 //         .enter().append("div")
 //             .style("width", function(d) {return d * 10 + "px"; })
+//             .style("background-color", "#F7B05B")
+//             .style("color", "#5E5B52")
 //             .text(function(d){return d; });
 // });
+//---------------Basic Bar Chart---------------
+
+//---------------Basic Bar Chart with method chaining---------------
+// window.addEventListener("load", function() {
+//     let chart = d3.select(".chart");
+
+//     let bar = chart.selectAll("div");
+
+//     let barUpdate = bar.data(data);
+
+//     barUpdate.enter().append("div")
+//         .style("width", function(d) { return d * 10 + "px"; })
+//         .style("background-color", "#738290")
+//         .style("color", "#FFFCF7")
+//         .style("text-align", "right")
+//         .style("padding-right", "5px")
+//         .style("border-bottom", "1px solid #FFFCF7")
+//         .text(function(d) { return d; });
+
+// });
+//---------------Basic Bar Chart with method chaining---------------
+
+//---------------Basic Bar Chart with a linear scale---------------
+// window.addEventListener("load", function() {
+//     let scale = d3.scaleLinear()
+//         .domain([0, d3.max(data)])
+//         .range([0, 420]);
+
+//     let chart = d3.select(".chart");
+
+//     let bar = chart.selectAll("div");
+
+//     let barUpdate = bar.data(data);
+
+//     barUpdate.enter().append("div")
+//         .style("width", function(d) { return scale(d) + "px"; })
+//         .style("background-color", "#738290")
+//         .style("color", "#FFFCF7")
+//         .style("text-align", "right")
+//         .style("padding-right", "5px")
+//         .style("border-bottom", "1px solid #FFFCF7")
+//         .text(function(d) { return d; });
+// });
+//---------------Basic Bar Chart with a linear scale---------------
 
 window.addEventListener("load", function() {
+    let scale = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([0, 420]);
+
     let chart = d3.select(".chart");
 
     let bar = chart.selectAll("div");
 
     let barUpdate = bar.data(data);
 
-    let barEnter = barUpdate.enter().append("div");
-    barEnter.style("width", function(d) { return d * 10+ "px"; });
-    barEnter.style("background-color", "#738290");
-    barEnter.style("color", "#FFFCF7");
-    barEnter.style("text-align", "right");
-    barEnter.style("padding-right", "5px");
-    barEnter.style("border-bottom", "1px solid #FFFCF7");
-    barEnter.text(function(d) { return d; });
-
+    barUpdate.enter().append("div")
+        .style("width", function(d) { return scale(d) + "px"; })
+        .style("background-color", "#738290")
+        .style("color", "#FFFCF7")
+        .style("text-align", "right")
+        .style("padding-right", "5px")
+        .style("border-bottom", "1px solid #FFFCF7")
+        .text(function(d) { return d; });
 });
